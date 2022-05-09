e6e59b00a8e5fab54ba3680109c1b060489a50c2
GOGNON Shira <gognonshira@gmail.com>
2022-05-09 15:48:41 +0200

    Remove duplication by using app layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,21 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('base')
 
-        <title>About us | Hello City</title>
-        
-    </head>
-    <body >
+@section('content')
 
     <p>Built with &hearts; by GOGNON Shira</p>
     <p><a href="/">Revenir à la page d'acceuil</a></p>
-    <!--date('d/m/y')-->
-
-    <footer>
-        <p>&copy; Copyright {{ date('Y')}} &middot;
-        </p>
-    </footer>
-    </body>
-</html>
+@endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,21 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('base')
 
-        <title>Home | Hello City</title>
-        
-    </head>
-    <body >
+@section('content')
 
     <h1>Hello from Bénin!</h1>
     <p>It's currently {{ date('h:i A')}}.</p>
     <!--date('d/m/y')-->
 
-    <footer>
-        <p>&copy; Copyright {{ date('Y')}} &middot; <a href="/about-us">About Us</a>
-        </p>
-    </footer>
-    </body>
-</html>
+@endsection
