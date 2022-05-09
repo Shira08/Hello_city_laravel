465041121f249c9ea16b9891444f4e2fb41ee3db
GOGNON Shira <gognonshira@gmail.com>
2022-05-09 16:04:58 +0200

    add proper titles

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,7 +1,9 @@
 @extends('base')
 
+@section('title','About Us | Hello City')
+
 @section('content')
 
     <p>Built with &hearts; by GOGNON Shira</p>
     <p><a href="/">Revenir à la page d'acceuil</a></p>
-@endsection
+@stop
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,4 +1,5 @@
 @extends('base')
+@section('title','Hello City')
 
 @section('content')
 
